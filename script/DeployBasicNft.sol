d545b954791671a480416ef7dc3cd020f33c7202
JordiMartinezDev <jordiupc87@gmail.com>
2023-12-28 08:48:36 +0100

    Script complete, compiling after typos fixed

--- script/DeployBasicNft.sol
+++ script/DeployBasicNft.sol
@@ -5,6 +5,16 @@ pragma solidity ^0.8.18;
 import {Script} from "../lib/forge-std/src/Script.sol";
 import {BasicNFT} from "../src/BasicNFT.sol";
 
-contract DeployBasicNft{
+contract DeployBasicNft is Script{
+
+
+    function run() external returns (BasicNFT){
+
+        vm.startBroadcast();
+        BasicNFT basicNft= new BasicNFT();
+        vm.stopBroadcast();
+
+        return basicNft;
+    }
 
 }
