fbcc94eba30271b9aa262e585fd61fcaf0fafa18
JordiMartinezDev <jordiupc87@gmail.com>
2023-12-28 08:32:18 +0100

    Mint NFT function

--- src/BasicNFT.sol
+++ src/BasicNFT.sol
@@ -7,13 +7,23 @@ import {ERC721} from "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC72
 contract basicNFT{
 
     uint private s_tokenCounter;
+    mapping(uint => string) private s_tokenIdToUri;
+
     constructor() ERC721("Patons","PTNS"){
 
         s_tokenCounter = 0;
     }
 
-    function mintNft() public{
-        
+    function mintNft(string memory tokenUri) public{
+
+        s_tokenIdToUri[s_tokenCounter] = tokenUri;
+
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+
     }
 
+    function tokenURI(uint tokenId) public view override returns (string memory){
+
+    }
 }
