035d3d481627f76a722aee25236d0d7e56dc008d
JordiMartinezDev <jordiupc87@gmail.com>
2023-12-29 17:32:57 +0100

    Mint function added

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -6,7 +6,24 @@ import {ERC721} from "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC72
 
 contract MoodNft is ERC721{
 
+    uint private s_tokenCounter;
+    string private s_sadSvg;
+    string private s_happySvg;
+
     constructor(string memory happySvg, string memory sadSvg) ERC721("Mood NFT", "MN"){
 
+        s_tokenCounter = 0;
+        s_sadSvg = sadSvg;
+        s_happySvg = happySvg;
+
+    }
+
+    function mintNft() public{
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
+
+    function tokenURI(uint tokenId) public view override returns(string memory){
+        
     }
 }
