0bb28414afc844c938d08b98d7a2e8848956a3e9
JordiMartinezDev <jordiupc87@gmail.com>
2023-12-30 08:14:18 +0100

    TokenUri creation function + Dynamic Mood

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -3,27 +3,62 @@
 pragma solidity ^0.8.18;
 
 import {ERC721} from "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "../lib/openzeppelin-contracts/contracts/utils/Base64.sol";
 
 contract MoodNft is ERC721{
 
     uint private s_tokenCounter;
-    string private s_sadSvg;
-    string private s_happySvg;
+    string private s_sadSvgImgUri;
+    string private s_happySvgImgUri;
 
+    enum Mood{
+        HAPPY,
+        SAD
+    }
+
+    mapping(uint => Mood) private s_tokenIdToMood;
     constructor(string memory happySvgImgUri, string memory sadSvgImgUri) ERC721("Mood NFT", "MN"){
 
         s_tokenCounter = 0;
-        s_sadSvg = sadSvgImgUri;
-        s_happySvg = happySvgImgUri;
+        s_sadSvgImgUri = sadSvgImgUri;
+        s_happySvgImgUri = happySvgImgUri;
 
     }
 
     function mintNft() public{
         _safeMint(msg.sender, s_tokenCounter);
+        s_tokenIdToMood[s_tokenCounter] = Mood.HAPPY;
         s_tokenCounter++;
     }
 
     function tokenURI(uint tokenId) public view override returns(string memory){
+
+        string memory imageUri;
+
+        if(s_tokenIdToMood[tokenId] == Mood.HAPPY){
+            imageUri = s_happySvgImgUri;
+        }else imageUri = s_sadSvgImgUri;
+            
+        
+        
+        return string(
+            abi.encodePacked(
+                _baseURI(),
+                Base64.encode(
+                    bytes( // bytes casting actually unnecessary as 'abi.encodePacked()' returns a bytes
+                        abi.encodePacked(
+                            '{"name":"',
+                            name(), // You can add whatever name here
+                            '", "description":"An NFT that reflects the mood of the owner, 100% on Chain!", ',
+                            '"attributes": [{"trait_type": "moodiness", "value": 100}], "image":"',
+                            imageURI,
+                            '"}'
+                        )
+                    )
+                )
+            )
+        );
+    }
 
     }
 }
