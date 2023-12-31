72de90eeeeed47c797ee763be6a76b14a12a9726
JordiMartinezDev <jordiupc87@gmail.com>
2023-12-31 12:35:56 +0100

    SvG to imgURI working

--- script/DeployMoodNft.s.sol
+++ script/DeployMoodNft.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.18;
 
 import {Script} from "../lib/forge-std/src/Script.sol";
 import {MoodNft} from "../src/MoodNft.sol";
+import {Base64} from "../lib/openzeppelin-contracts/contracts/utils/Base64.sol";
 
 contract DeployMoodNft is Script{
 
@@ -16,6 +17,11 @@ contract DeployMoodNft is Script{
     }
 
     function svgToImgURI(string memory svg) public pure returns (string memory){
+
+        string memory baseURL = "data:image/svg+xml;base64,";
+        string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
+
+        return string(abi.encodePacked(baseURL, svgBase64Encoded));
         
     }
 
